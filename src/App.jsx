69326f77bc9ff805ace7092fdcd8b2333900f68b
gaeVG <dev@cenne.xyz>
2024-09-13 05:07:55 +0200

    add:(app context)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   Routes,
   useLocation,
 } from "react-router-dom";
-import { useState, useRef } from "react";
+import { useState, createContext } from "react";
 import { AnimatePresence } from "framer-motion";
 import MainPage from "./components/MainPage/MainPage";
 import About from "./Pages/About/About";
@@ -16,74 +16,56 @@ import Navbar from "./components/NavBar/Navbar";
 import Clip from "./Pages/Clip/Clip";
 import Header from "./components/Header/Header";
 import Join from "./Pages/Join/Join";
-import sonDay from "./assets/son/ZIK BOUCLE JOUR.mp3";
-import sonNight from "./assets/son/ZIK NUIT.mp3";
 import Lecteur from "./components/Lecteur/Lecteur"; // Importer le composant Lecteur
 import Error from "./Pages/Error/Error";
 import Home from "./Pages/Home/Home";
+
+export const AppContext = createContext();
 
 const App = () => {
   const location = useLocation();
   const [isPlaying, setIsPlaying] = useState(false);
   const [isDay, setIsDay] = useState(true);
-  const audioRef = useRef(null);
-
-  const togglePlayPause = () => {
-    const audio = audioRef.current;
-    if (isPlaying) {
-      audio.pause();
-    } else {
-      audio.play();
-    }
-    setIsPlaying(!isPlaying);
-    console.log(isPlaying, "dans app");
-  };
 
   return (
-    <div className="flex flex-col h-[100vh] justify-between items-center">
-      {location.pathname === "/home" || location.pathname === "/" ? (
-        <></>
-      ) : (
-        <Header />
-      )}
+    <AppContext.Provider
+      value={{
+        isPlaying,
+        setIsPlaying,
+        isDay,
+        setIsDay,
+      }}
+    >
+      <div className="flex flex-col h-[100vh] justify-between items-center">
+        {location.pathname === "/home" || location.pathname === "/" ? (
+          <></>
+        ) : (
+          <Header />
+        )}
 
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/home"
-            element={
-              <MainPage
-                isPlaying={isPlaying}
-                togglePlayPause={togglePlayPause}
-                isDay={isDay}
-                setIsDay={setIsDay}
-              />
-            }
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/livre" element={<Livre />} />
-          <Route path="/concert" element={<Concert />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/clip" element={<Clip />} />
-          <Route path="*" element={<NotFound />} />{" "}
-        </Routes>
-      </AnimatePresence>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<MainPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/livre" element={<Livre />} />
+            <Route path="/concert" element={<Concert />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/clip" element={<Clip />} />
+            <Route path="*" element={<NotFound />} />{" "}
+          </Routes>
+        </AnimatePresence>
 
-      {location.pathname === "/home" || location.pathname === "/" ? (
-        <></>
-      ) : (
-        <Navbar />
-      )}
-      <Cursor />
-      <Lecteur
-        isPlaying={isPlaying}
-        togglePlayPause={togglePlayPause}
-        audioRef={audioRef}
-        son={isDay ? sonDay : sonNight}
-      />
-    </div>
+        {location.pathname === "/home" || location.pathname === "/" ? (
+          <></>
+        ) : (
+          <Navbar />
+        )}
+        <Cursor />
+        <Lecteur />
+      </div>
+    </AppContext.Provider>
   );
 };
 
