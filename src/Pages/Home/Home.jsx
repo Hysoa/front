8dca8147583c890b984b5e632853ad07bcadeccb
gaeVG <dev@cenne.xyz>
2024-09-14 00:44:31 +0200

    remove:(enter in full screen); update:(types node module)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,13 @@
 import "./home.css";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AppContext } from "../../App";
 import homeImage from "../../assets/images/logo/LOGO HYSOA DEFINITIF.png";
 
 export default function Home() {
   const navigate = useNavigate();
-  const { mainRef } = useContext(AppContext);
-
-  const enterFullscreen = (element) => {
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-      element.msRequestFullscreen();
-    }
-  };
 
   const handleClick = () => {
-    if (mainRef.current) {
-      enterFullscreen(mainRef.current);
-    }
     navigate("/home");
   };
-
 
   return (
     <div className="overflow-hidden relative h-screen flex justify-center">
