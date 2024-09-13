be48e25aa7c45935b57799b75591aee700d8c66b
gaeVG <dev@cenne.xyz>
2024-09-13 05:07:43 +0200

    fix:(audio playback when changing background)

--- src/components/Lecteur/Lecteur.jsx
+++ src/components/Lecteur/Lecteur.jsx
@@ -1,16 +1,42 @@
 import "./lecteur.css";
+import { useRef, useContext, useEffect, useState } from "react";
 
-import play from "./../../assets/images/lecteur/BOUTON LECTURE.png";
-import pause from "./../../assets/images/lecteur/BOUTON PAUSE.png";
+import play from "../../assets/images/lecteur/BOUTON LECTURE.png";
+import pause from "../../assets/images/lecteur/BOUTON PAUSE.png";
 
-export default function Lecteur({ isPlaying, togglePlayPause, audioRef, son }) {
+import sonDay from "./../../assets/son/ZIK BOUCLE JOUR.mp3";
+import sonNight from "./../../assets/son/ZIK NUIT.mp3";
+
+import { AppContext } from "../../App";
+
+export default function Lecteur() {
+  const { isPlaying, isDay } = useContext(AppContext);
+  const [son, setSon] = useState(null);
+  const audioRef = useRef(null);
+
+  const togglePlayPause = (e) => {
+    e.preventDefault();
+    const audio = audioRef.current;
+    isPlaying ? audio.pause() : audio.play();
+  };
+
+  useEffect(() => setSon(isDay ? sonDay : sonNight), [isDay]);
+
+  useEffect(() => {
+    if (!son) return;
+    const audio = audioRef.current;
+    audio.src = son;
+    audio.load();
+    if (isPlaying) {
+      audio.play();
+    }
+  }, [son, isPlaying]);
+
   return (
     <div className="player">
-      <audio ref={audioRef} src={son} loop />
+      <audio ref={audioRef} loop />
       <div className="controls">
-        <button onClick={togglePlayPause}>
-          <img src={!isPlaying ? play : pause} alt="" />
-        </button>
+        <img src={!isPlaying ? play : pause} alt="" onClick={togglePlayPause} />
       </div>
     </div>
   );
