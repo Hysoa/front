d8d04cbc3fd573ab9e08544405fdb5dce38f29fa
gaeVG <any@cenne.xyz>
2025-09-21 20:08:22 +0200

    add:(new album)

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -6,8 +6,10 @@ import "./shop.css";
 import bg from "./../../assets/images/menu/FOND VIDE.png";
 import sleepwel from "./../../assets/images/album/sleepwell.jpg";
 import itcould from "./../../assets/images/album/itcould.jpg";
+import itwill from "./../../assets/images/album/itwill.png";
 import detailSleep from "./../../assets/images/album/details/sleepwell.png";
-import detailIt from "./../../assets/images/album/details/itcould.png";
+import detailItCould from "./../../assets/images/album/details/itcould.png";
+import detailItWill from "./../../assets/images/album/details/itwill.png";
 import fleche from "./../../assets/images/buttons/fleche.png";
 
 export default function Shop() {
@@ -76,7 +78,6 @@ export default function Shop() {
     setReceivedLinks(true);
   }, []);
 
-
   useEffect(() => {
     switch (selectedAlbum) {
       case "sleepwell":
@@ -84,8 +85,12 @@ export default function Shop() {
         setLinkToListen("https://youtu.be/3PL75Iz0oR8?si=gtvTG0bsw7AyxQzN");
         break;
       case "itcould":
-        setCover(detailIt);
+        setCover(detailItCould);
         setLinkToListen("https://youtu.be/5LBdYEu1KhA?si=EPPtMLrEZTTGnuk8");
+        break;
+      case "itwill":
+        setCover(detailItWill);
+        setLinkToListen("https://youtu.be/8X2C3NQ0xdU?si=OzExmvNFmn97oDZ3");
         break;
       default:
         setCover(null);
@@ -164,18 +169,24 @@ export default function Shop() {
               dangerouslySetInnerHTML={{ __html: purshaseValidationMessage }}
             />
           )}
-          <div className="absolute flex justify-between top-[12vw] left-[28vw] 4xl:top-[6vw] 4xl:left-[17.5vw] w-[44vw] 4xl:w-[20vw] gap-[1em]">
+          <div className="absolute flex flex-wrap justify-center top-[14vw] left-[25vw] 4xl:top-[6vw] 4xl:left-[17.5vw] w-[51vw] 4xl:w-[20vw] gap-[1em]">
             <img
               src={sleepwel}
               onClick={() => handleAlbumClick("sleepwell")}
               alt="Sleepwell Album"
-              className="h-[20vw] w-[20vw] 4xl:w-[16vw] 4xl:h-[16vw] hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+              className="h-[16vw] w-[16vw] 4xl:w-[16vw] 4xl:h-[16vw] hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
             />
             <img
               src={itcould}
               onClick={() => handleAlbumClick("itcould")}
               alt="It Could Album"
-              className="h-[20vw] w-[20vw] 4xl:w-[16vw] 4xl:h-[16vw] hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+              className="h-[16vw] w-[16vw] 4xl:w-[16vw] 4xl:h-[16vw] hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+            />
+            <img
+              src={itwill}
+              onClick={() => handleAlbumClick("itwill")}
+              alt="It Could Album"
+              className="h-[16vw] w-[16vw] 4xl:w-[16vw] 4xl:h-[16vw] hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
             />
           </div>
         </section>
@@ -184,9 +195,18 @@ export default function Shop() {
       {selectedAlbum && (
         <section className="album__details-container">
           <section className="album__details relative">
-            <img className="w-[70vw] xl:w-[50vw]" src={cover} alt="Sleepwell Details" />
+            <img
+              className="w-[70vw] xl:w-[50vw]"
+              src={cover}
+              alt="Sleepwell Details"
+            />
             <div className="album__navigation top-0">
-              <img onClick={handleReturn} src={fleche} alt="Arrow" className="absolute top-0 left-[-8vw] xl:top-[-1vw]" />
+              <img
+                onClick={handleReturn}
+                src={fleche}
+                alt="Arrow"
+                className="absolute top-0 left-[-8vw] xl:top-[-1vw]"
+              />
               <div className="absolute flex text-white left-[-6vw] xl:left-[-4vw] top-[41vw] xl:top-[29vw] !p-1 2xl:!p-3 4xl:!p-7 text-lg lg:text-xl 2xl:text-3xl 4xl:text-6xl bg-zinc-900/20 xl:bg-black/50 justify-between">
                 <div
                   onClick={() => window.open(linkToListen, "_blank")}
@@ -194,40 +214,42 @@ export default function Shop() {
                 >
                   Ecouter
                 </div>
-                <div className="relative !p-1 lg:!p-2">
-                  <span
-                    id="purshase"
-                    onClick={() => setSelectPurshase(!selectPurshase)}
-                    className={cn(
-                      "relative",
-                      "hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]",
-                      selectPurshase &&
-                        "drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
-                    )}
-                  >
-                    Acheter
-                  </span>
-
-                  {selectPurshase && (
-                    <ul
-                      ref={purshaseSelectRef}
-                      className="absolute bottom-[2.8em] left-[-0.2em] w-[9em] bg-black/50 p-3 4xl:p-6 rounded-xl"
+                {selectedAlbum !== "itwill" && (
+                  <div className="relative !p-1 lg:!p-2">
+                    <span
+                      id="purshase"
+                      onClick={() => setSelectPurshase(!selectPurshase)}
+                      className={cn(
+                        "relative",
+                        "hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]",
+                        selectPurshase &&
+                          "drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+                      )}
                     >
-                      <li
-                        className="hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
-                        onClick={() => handleBuy("shipping-france")}
+                      Acheter
+                    </span>
+
+                    {selectPurshase && (
+                      <ul
+                        ref={purshaseSelectRef}
+                        className="absolute bottom-[2.8em] left-[-0.2em] w-[9em] bg-black/50 p-3 4xl:p-6 rounded-xl"
                       >
-                        Livraison (France)
-                      </li>
-                      <li
-                        className="hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
-                        onClick={() => handleBuy("shipping-international")}
-                      >
-                        Livraison (International)
-                      </li>
-                    </ul>
-                  )}
-                </div>
+                        <li
+                          className="hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+                          onClick={() => handleBuy("shipping-france")}
+                        >
+                          Livraison (France)
+                        </li>
+                        <li
+                          className="hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]"
+                          onClick={() => handleBuy("shipping-international")}
+                        >
+                          Livraison (International)
+                        </li>
+                      </ul>
+                    )}
+                  </div>
+                )}
               </div>
             </div>
           </section>
